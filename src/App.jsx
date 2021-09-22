4febc197abf0c900ce8b2e4edd7069a38a76b240
hernan066 <hernan0066@gmail.com>
2021-09-22 01:36:47 -0300

    1.0.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="container">
-     <h1>Auth</h1>
-     <hr />
-     
-    </div>
+    
+    <Router>
+        <div className="container">
+          navbar...
+            <Switch>
+                <Route path="/login">
+                  login...
+                </Route>
+                <Route path="/admin">
+                  admin...
+                </Route>
+                <Route path="/">
+                  Inicio...
+                </Route>
+            </Switch>
+        </div>
+      
+    </Router>
   );
 }
 
