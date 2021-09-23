c1f02044921f1df9297edfd899a0677be57bee58
hernangit config --global user.name John <hernan0066@gmail.com>
2021-09-23 00:20:11 -0300

    1.0.14

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import { Login } from './components/Login';
+import Login from './components/Login'
 import { Navbar } from './components/Navbar';
 
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,115 +1,156 @@
 import React, { useCallback, useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import  app  from "../firebase";
 import { getFirestore, doc, setDoc} from "firebase/firestore"
+import { withRouter } from "react-router";
 
-export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
-  const [error, setError] = useState(null);
-  const [esRegistro, setEsRegistro] = useState(true);
-   
+const Login = (props) => {
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const [error, setError] = useState(null);
+    const [esRegistro, setEsRegistro] = useState(true);
+     
+    
+    
+    
+    
+    //procesarDatos
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (!email.trim()) {
+        //console.log('Ingrese email');
+        setError("Ingrese email");
+        return;
+      }
+      if (!pass.trim()) {
+        //console.log('Ingrese password');
+        setError("Ingrese password");
+        return;
+      }
+      if (pass.length < 6) {
+        //console.log('El password debe tener 6 caracteres minimo');
+        setError("El password debe tener 6 caracteres minimo");
+        return;
+      }
+      setError(null);
   
-  
-  
-  
-  //procesarDatos
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!email.trim()) {
-      //console.log('Ingrese email');
-      setError("Ingrese email");
-      return;
-    }
-    if (!pass.trim()) {
-      //console.log('Ingrese password');
-      setError("Ingrese password");
-      return;
-    }
-    if (pass.length < 6) {
-      //console.log('El password debe tener 6 caracteres minimo');
-      setError("El password debe tener 6 caracteres minimo");
-      return;
-    }
-    setError(null);
-
-    if(esRegistro){
-        registrar();
+      if(esRegistro){
+          registrar();
+      }else{
+          login();
+      };
     };
-  };
-
-const registrar = useCallback(async()=>{
-    try {
-        const auth = getAuth(app);
-        const db = getFirestore(app);
-        const res = await createUserWithEmailAndPassword(auth, email, pass);
-        console.log(res.user)
-        
-        await setDoc(doc(db, "usuarios", res.user.uid), {
-            email: res.user.email,
-            uid: res.user.uid
-          });
+  
+      const login = useCallback(
+          async() => {
+              try {
+                  //login de usuario
+                  const auth = getAuth(app);
+                  const res =   await signInWithEmailAndPassword(auth,email, pass);
+                 
+                  console.log(res.user)
+  
+                  //limpia formulario
+                  setEmail('');
+                  setPass('');
+                  setError(null);
+                  props.history.push('/admin');
+              
+              } catch (error) {
+                  //console.log(error);
+                  if(error.code === 'auth/user-not-found'){
+                      setError('Usuario o contraseña incorrecta')
+                  };
+                  if(error.code === 'auth/wrong-password'){
+                      setError('Usuario o contraseña incorrecta')
+                  };
+              }
+          },
+          [email, pass, props.history]
+      );
+  
+  
+  
+    const registrar = useCallback(async()=>{
+      try {
+          //crea nuevo usuario
+          const auth = getAuth(app);
+          const db = getFirestore(app);
+          const res = await createUserWithEmailAndPassword(auth, email, pass);
+          //console.log(res.user)
           
-        
-        setEmail('')
-        setPass('')
-        setError(null)
-    } catch (error) {
-        console.log(error)
-        if(error.code === 'auth/invalid-email'){
-            setError('Email no válido')
-            return
-        }
-        if(error.code === 'auth/email-already-in-use'){
-            setError('Usuario ya registrado...')
-            return
-        }
-    }
-}, [email, pass]);
-
-  return (
-    <div className="mt-5">
-      <h3 className="text-center">
-        {esRegistro ? "Registro de usuarios" : "Login de acceso"}
-      </h3>
-      <hr />
-      <div className="row justify-content-center">
-        <div className="col-12 col-sm-8 col-md-6 col-xl-4">
-          <form onSubmit={handleSubmit}>
-            {error && <div className="alert alert-danger">{error}</div>}
-
-            <input
-              className="form-control mb-2"
-              type="email"
-              placeholder="Ingrese un email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-            />
-            <input
-              className="form-control mb-2"
-              type="password"
-              placeholder="Ingrese un password"
-              onChange={(e) => setPass(e.target.value)}
-              value={pass}
-            />
-
-            <div className="d-grid gap-2">
-              <button className="btn btn-dark btn-lg mb-2" type="submit">
-                {esRegistro ? "Registrarse" : "Ingresar"}
-              </button>
-            </div>
-            <div className="d-grid gap-2">
-              <button
-                className="btn btn-info btn-sm "
-                onClick={() => setEsRegistro(!esRegistro)}
-                type="button"
-              >
-                {esRegistro ? "Ya estas registrado?" : "No tienes cuenta?"}
-              </button>
-            </div>
-          </form>
+          //crea la coleccion usuarios
+          await setDoc(doc(db, "usuarios", res.user.uid), {
+              email: res.user.email,
+              uid: res.user.uid
+            });
+            
+          //limpia formulario
+          setEmail('')
+          setPass('')
+          setError(null)
+          props.history.push('/admin');
+  
+      } catch (error) {
+          //console.log(error)
+          if(error.code === 'auth/invalid-email'){
+              setError('Email no válido')
+              return
+          }
+          if(error.code === 'auth/email-already-in-use'){
+              setError('Usuario ya registrado...')
+              return
+          }
+      }
+  }, [email, pass, props.history]);
+  
+    
+  
+      return (
+      <div className="mt-5">
+        <h3 className="text-center">
+          {esRegistro ? "Registro de usuarios" : "Login de acceso"}
+        </h3>
+        <hr />
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-8 col-md-6 col-xl-4">
+            <form onSubmit={handleSubmit}>
+              {error && <div className="alert alert-danger">{error}</div>}
+  
+              <input
+                className="form-control mb-2"
+                type="email"
+                placeholder="Ingrese un email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+              <input
+                className="form-control mb-2"
+                type="password"
+                placeholder="Ingrese un password"
+                onChange={(e) => setPass(e.target.value)}
+                value={pass}
+              />
+  
+              <div className="d-grid gap-2">
+                <button className="btn btn-dark btn-lg mb-2" type="submit">
+                  {esRegistro ? "Registrarse" : "Ingresar"}
+                </button>
+              </div>
+              <div className="d-grid gap-2">
+                <button
+                  className="btn btn-info btn-sm "
+                  onClick={() => setEsRegistro(!esRegistro)}
+                  type="button"
+                >
+                  {esRegistro ? "Ya estas registrado?" : "No tienes cuenta?"}
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    )
+}
+
+export default withRouter(Login)
